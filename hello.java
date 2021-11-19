6a25d0a587610b1cff456b2f321596f98a1fd156
jessica-robyn01 <82172397+jessica-robyn01@users.noreply.github.com>
2021-11-19 12:10:30 -0500

    stylin'

---
+++ hello.java
@@ -0,0 +1,14 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  //autoPlay:1000
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
